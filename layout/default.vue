ab7715a8e29d3206c84d95434807cae05f4b8a93
Micha Kraußer <michak@synphonyte.com>
2022-09-05 09:48:48 -0600

    set up fetched data and showd it up in front end

---
+++ layout/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <NuxtLink to="/">Home</NuxtLink>
+  </nav>
+  <main>
+    <slot />
+  </main>
+</template>
+
+<script>
+export default {
+  name: "default"
+}
+</script>
+
+<style scoped>
+
+</style>
